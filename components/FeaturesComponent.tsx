d08879057f05fd7941bb5cc8b33ab46736089d5c
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-06-23 00:30:51 +0530

    FeaturesComponent and Features-layout mob size Adding

--- components/FeaturesComponent.tsx
+++ components/FeaturesComponent.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+"use client"
+import { ChevronDown } from "lucide-react";
+import React, { useState } from "react";
 
 interface FeaturesProps {
-    title?: string |number;
+    title?: string | number;
     content?: string;
 }
 
-const FeaturesComponent: React.FC<FeaturesProps> = ({ title, content}) => {
+const FeaturesComponent: React.FC<FeaturesProps> = ({ title, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleContent = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-    <div>
-    <div className="mt-4">
-      <div className="text-h2 text-[#0E462C] w-auto max-w-[200px]">{title}</div>
-      </div>
-      <p className="mt-2 w-auto max-w-[380px]">{content}</p>
-    </div>
+        <div className="mt-4">
+            <div className="flex justify-between items-center cursor-pointer" onClick={toggleContent}>
+                <div className="text-large lg:text-h2 text-[#0E462C] w-auto ">{title}</div>
+                <div className={`transform ${isOpen ? 'rotate-180' : ''} transition-transform sm:hidden`}>
+                    <ChevronDown style={{ color: '#0E462C' }} />
+                </div>
+            </div>
+            <p className={`mt-2 w-auto max-w-[380px] text-[15px] ${isOpen ? 'block' : 'hidden'} sm:block`}>
+                {content}
+            </p>
+        </div>
     );
-  };
-  
-  export default FeaturesComponent;
-  
+};
+
+export default FeaturesComponent;
