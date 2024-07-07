16ce1068fb2d8dcb2a017add3148e07229bbe622
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-07-07 19:38:32 +0530

    commit changes..Hero layout

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -1,30 +1,19 @@
-"use client";
 import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <div
-      style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",}}>
-      <div className="lg:text-h1 sm:text-h2 text-center">Your plants need to be</div>
-      <div className="lg:text-h1 sm:text-h2 text-center">Taken care of.</div>
-      <img
+    <div>
+      <div className="text-h2 lg:text-h1 text-center">
+        <div>Your plants need to be</div>
+        <div>Taken care of.</div>
+      </div>
+     <img
         src="/images/Hero_image.png"
         alt="Hero"
-        width="256"
-        height="527"
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-        }}
+        width="104px"
+        height="216px"
+        className="lg:w-64 lg:h-132 justify-center items-center mx-auto"
       />
-      <style jsx>{`
-        @media (max-width: 640px) {
-          img {
-            width: 105px;
-            height: 216px;
-          }
-        }
-      `}</style>
     </div>
   );
 };
