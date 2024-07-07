7321944807abd4c1bc6ae62224a139c96280683f
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-07-07 15:38:12 +0530

    first commit

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -1,13 +1,32 @@
+"use client";
 import React from "react";
 
 const Hero: React.FC = () => {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-            <div className="text-h1">Your plants need to be</div>
-            <div className="text-h1 text-center">Taken care of.</div>
-            <img src="/images/Hero_image.png" alt="Hero" width="256px" height="527px" />
-        </div>
-    );
+  return (
+    <div
+      style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",}}>
+      <div className="lg:text-h1 sm:text-h2 text-center">Your plants need to be</div>
+      <div className="lg:text-h1 sm:text-h2 text-center">Taken care of.</div>
+      <img
+        src="/images/Hero_image.png"
+        alt="Hero"
+        width="256"
+        height="527"
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+      />
+      <style jsx>{`
+        @media (max-width: 640px) {
+          img {
+            width: 105px;
+            height: 216px;
+          }
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Hero;
