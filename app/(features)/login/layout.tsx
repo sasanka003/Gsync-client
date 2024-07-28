996b2afab8cf54398f37ca0b495102ae3e304c6d
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-07-28 12:24:56 +0530

    Changes

--- app/(features)/login/layout.tsx
+++ app/(features)/login/layout.tsx
@@ -1,6 +1,4 @@
 import LoginNav from "./components/LoginNav";
-import NotificationCard from "../../../components/NotificaionCard";
-import UpgradeAccountCard from "@/components/UpgradeAccountCard";
 
 export default function AuthLayout({
   children,
