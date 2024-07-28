06bd758238ef575761dc9400cad9506de105b4f3
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-07-28 12:24:14 +0530

    Login page and few changes in create account

--- app/(features)/login/components/Login.tsx
+++ app/(features)/login/components/Login.tsx
@@ -15,11 +15,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { EyeOff, Eye } from "lucide-react";
+import { Checkbox } from "../../../../components/ui/checkbox";
 
 const schema = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.string().email("Invalid email address"),
-  contactNumber: z.string().min(1, "Contact number is required"),
   username: z.string().min(1, "Username is required"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
@@ -28,9 +26,6 @@ const CreateAccountForm = () => {
   const form = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
-      name: "",
-      email: "",
-      contactNumber: "",
       username: "",
       password: "",
     },
@@ -46,77 +41,25 @@ const CreateAccountForm = () => {
   return (
     <div className="lg:w-[715px] md:w-[664px] mx-auto p-4">
       <div className="mb-5">
-        <h1 className="text-h2 font-bold md:text-h1 md:font-bold text-center text-common">Create an Account</h1>
+        <h1 className="text-h2 font-bold md:text-h1 md:font-bold text-center text-common">
+          Create an Account
+        </h1>
         <div className="text-subtle text-center text-accent-foreground md:text-p">
           Start today with creating your free account
         </div>
       </div>
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  <div className="text-subtle-medium md:text-p text-common">Name*</div>
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="Enter your name (John Doe)"
-                    className="placeholder:text-p"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  <div className="text-subtle-medium md:text-p text-common">Email* </div>
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="Enter your email (yourname@example.com)"
-                    className="placeholder:text-p"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="contactNumber"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  <div className="text-subtle-medium md:text-p text-common">Contact number* </div>
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="+94 77 123 4567"
-                    className="placeholder:text-p"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>
-                  <div className="text-subtle-medium md:text-p text-common">Username* </div>
+                  <div className="text-subtle-medium md:text-p text-common">
+                    Username*
+                  </div>
                 </FormLabel>
                 <FormControl>
                   <Input
@@ -129,6 +72,7 @@ const CreateAccountForm = () => {
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="password"
@@ -136,7 +80,9 @@ const CreateAccountForm = () => {
               <FormItem>
                 <FormLabel>
                   <div className="flex items-center justify-between">
-                    <div className="text-subtle-medium md:text-p text-common">Create Password*</div>
+                    <div className="text-subtle-medium md:text-p text-common">
+                      Password*
+                    </div>
                     <button
                       type="button"
                       className="mr-1 flex items-center text-common"
@@ -159,14 +105,11 @@ const CreateAccountForm = () => {
                     />
                   </div>
                 </FormControl>
-                <div className="text-body text-muted-foreground mt-1">
-                  Use 8 or more characters with a mix of letters, numbers &
-                  symbols
-                </div>
                 <FormMessage />
               </FormItem>
             )}
           />
+
           <div>
             <span className="text-common">
               By creating an account, you agree to our{" "}
@@ -175,34 +118,44 @@ const CreateAccountForm = () => {
               </a>
             </span>
           </div>
+          <div className="flex justify-between items-center">
+          <div className="flex gap-2">
+            <Checkbox className="w-4 h-4 bg-fill" />
+           <div className=" text-small">Keep me logged in</div> 
+          </div>
+          <div className="text-LM underline text-text">Forgot Password?</div></div>
           <div>
             <Button
               type="submit"
-              className="w-full text-body-medium text-fill dark:text-common mt-4"
+              className="w-full text-body-medium text-fill dark:text-common mt-2"
             >
-              Create Account
+              Log In
             </Button>
           </div>
         </form>
       </Form>
       <div className="mt-8 text-center">
-        <p className="mb-4">Or Continue with</p>
+        <p className="mb-4 text-common">Or Continue with</p>
         <div className="flex justify-center space-x-6 mt-2">
-          <button className="flex items-center px-4 py-2 bg-fill rounded text-suble-semibold lg:w-[220px] gap-2">
+          <button className="flex items-center px-4 py-2 bg-fill rounded text-suble-semibold lg:w-[220px] gap-2 text-common">
             <img
               src="/images/GoogleIcon.png"
               alt="Google icon"
               className="w-6 h-6"
             />
-            <span className="ml-2 inline lg:block md:hidden sm:hidden dark:text-fill">Sign in with Google</span>
+            <span className="ml-2 inline lg:block md:hidden sm:hidden dark:text-fill">
+              Sign in with Google
+            </span>
           </button>
-          <button className="flex items-center px-4 py-2 bg-common rounded text-fill text-suble-semibold lg:w-[220px] gap-2">
+          <button className="flex items-center px-4 py-2 bg-common rounded text-fill text-suble-semibold lg:w-[220px] gap-2 text-common">
             <img
               src="/images/AppleIcon.png"
               alt="Apple icon"
               className="w-6 h-6"
             />
-            <span className="ml-2 inline lg:block md:hidden sm:hidden text-fill dark:text-common">Sign in with Apple</span>
+            <span className="ml-2 inline lg:block md:hidden sm:hidden text-fill dark:text-common">
+              Sign in with Apple
+            </span>
           </button>
           <button className="flex items-center px-4 py-2 bg-[#1877F2] rounded text-suble-semibold lg:w-[220px] gap-2 text-common">
             <img
@@ -210,7 +163,9 @@ const CreateAccountForm = () => {
               alt="Facebook icon"
               className="w-6 h-6"
             />
-            <span className="ml-2 inline lg:block md:hidden sm:hidden text-fill dark:text-common">Sign in with Facebook</span>
+            <span className="ml-2 inline lg:block md:hidden sm:hidden text-fill dark:text-common">
+              Sign in with Facebook
+            </span>
           </button>
         </div>
         <p className="mt-6 text-common">
