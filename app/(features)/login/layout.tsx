bc2c7ab937eb01435dc133304b7fcf59c7d394e5
senudi Disakya <2021cs123@stu.ucsc.cmb.ac.lk>
2024-07-28 11:23:30 +0530

    NotificationCard and UpgradeAccountCard

--- app/(features)/login/layout.tsx
+++ app/(features)/login/layout.tsx
@@ -1,4 +1,6 @@
 import LoginNav from "./components/LoginNav";
+import NotificationCard from "../../../components/NotificaionCard";
+import UpgradeAccountCard from "@/components/UpgradeAccountCard";
 
 export default function AuthLayout({
   children,
